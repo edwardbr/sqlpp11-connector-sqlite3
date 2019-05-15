03562ae979f6b690c36ad2d0323dde0dd6a6f0ce
edward <edward@boggis-rolfe.com>
2019-05-15 16:41:55 +0100

    fixed dependencies for sqlite

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,1 +1,7 @@
 hunter_config(OpenSSL VERSION 1.1.0j)
+hunter_config(Sqlpp11 
+    URL https://github.com/hunter-packages/sqlpp11/archive/v0.57-p0.tar.gz
+    SHA1 472f23489f9a063d2b9a492910ea59a36bc55263)
+hunter_config(sqlcipher
+    URL https://github.com/edwardbr/sqlcipher/archive/hunter-3.27.2.tar.gz
+    SHA1 2c168e1566236b78b2b420f356f34947b5a20925)
