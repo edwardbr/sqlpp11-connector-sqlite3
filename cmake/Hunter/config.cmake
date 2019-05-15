c835802d406e265c54cb9aea8dfd3d9dcf8496f1
edward <edward@boggis-rolfe.com>
2019-05-15 19:01:28 +0100

    tweeked dependancies

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,4 +4,4 @@ hunter_config(Sqlpp11
     SHA1 472f23489f9a063d2b9a492910ea59a36bc55263)
 hunter_config(sqlcipher
     URL https://github.com/edwardbr/sqlcipher/archive/hunter-3.27.2.tar.gz
-    SHA1 2c168e1566236b78b2b420f356f34947b5a20925)
+    SHA1 7c2957823c6e1c16bbe61d08a8b305f00542b66c)
